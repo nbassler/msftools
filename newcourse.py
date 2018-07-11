2f699363dfd09656da34902619ec653689f72357
Niels Bassler <niels.bassler@fysik.su.se>
2018-07-11 16:01:15 +0200

    Initial import.

---
+++ newcourse.py
@@ -0,0 +1,81 @@
+import sys
+import logging
+import argparse
+import pytz  # timezones
+
+from icalendar import Calendar
+from icalendar import Event
+from datetime import datetime
+from datetime import timedelta
+
+
+logger = logging.getLogger(__name__)
+
+# local_tz = pytz.timezone('CET')
+local_tz = pytz.timezone('Europe/Stockholm')  # beware of daylight saving
+
+
+def utc_to_local(utc_dt):
+    local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
+    return local_tz.normalize(local_dt)  # .normalize might be unnecessary
+
+
+def main(args=sys.argv[1:]):
+    """
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbosity", action='count',
+                        help="increase output verbosity",
+                        default=0)
+    parsed_args = parser.parse_args(args)
+
+    if parsed_args.verbosity == 1:
+        logging.basicConfig(level=logging.INFO)
+    elif parsed_args.verbosity > 1:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig()
+
+    fn_in = "test.ics"
+    fn_out = "out.ics"
+
+    new_start_date = datetime(2018, 11, 19, 9, 0, 0, tzinfo=local_tz)
+    old_start_date = new_start_date
+
+    with open(fn_in, 'rb') as g:
+        gcal = Calendar.from_ical(g.read())
+
+    # first scan for the oldest date in the calendar
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            # print(component['summary'].dt)
+            _d = component['dtstart'].dt
+            if _d < old_start_date:
+                old_start_date = _d
+
+    delta = new_start_date - old_start_date
+    delta = timedelta(days=delta.days)
+    logger.info("Last years course started {}".format(utc_to_local(old_start_date)))
+    logger.info("New course will be {} days later.".format(delta.days))
+
+    new = old_start_date + delta
+    logger.info("Course start: {}".format(utc_to_local(new)))
+
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            logger.info("")
+            logger.info("{}".format(component['summary']))
+            logger.info("Old date: {}".format(component['dtstart'].dt))
+            component['dtstart'].dt += delta
+            logger.info("New date: {}".format(component['dtstart'].dt))
+            if "dtend" in component:
+                component['dtend'].dt += delta
+            component['dtstamp'].dt = datetime.now()
+
+    with open(fn_out, 'wb') as f:
+        f.write(gcal.to_ical())
+    logger.info("Wrote {}".format(fn_out))
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
